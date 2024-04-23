c16d86878ca58447c9d4f57fb0c8a55f0b05fa89
Katia Molina <zs19014027@estudiantes.uv.mx>
2024-04-23 01:10:27 -0600

    Creacion de la barra del menu
    * Implemente el flujo de pantallas

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,87 @@
 import { useState } from 'react'
 import { Button, Flex, Box, Container, Image, Avatar, Text } from "@chakra-ui/react";
+import { FaHome, FaBox, FaFileAlt, FaTools, FaPowerOff, FaChartBar } from 'react-icons/fa';
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { theme } from './estilos/theme';
+import { buttonStyle } from './estilos/buttonStyle';
+import Dashboard from './paginas/Dashboard';
 
+const EnProcesoContent = () => <Box>En proceso de construccion :)!</Box>;
 
 function App() {
+  
+  const [activeCategory, setActiveCategory] = useState("dashboard");
 
+  const changeCategory = (category) => () => {
+    setActiveCategory(category);
+  };
+
+  const renderContent = () => {
+    switch (activeCategory) {
+      case "dashboard":
+        return <Dashboard />;
+      case "enProceso":
+        return <EnProcesoContent />;
+    }
+  };
 
   return (
-    <>
-      <Flex height="100vh">
-        <Box flex="0 0 12%" display='flex' flexDirection='column' alignItems='center' backgroundImage="../src/assets/img/nav_background.png" backgroundSize='250px' backgroundPosition="left bottom" backgroundRepeat='no-repeat'>
-          <Image src='/src/assets/img/dif_logo.png' h='50px' alt='DIF Xalapa' marginY='50px' />
-          <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov' size='2xl' marginBottom='10px' />
-          <Text fontSize='lg' color='#310E3A'>Nombre Usuario</Text>
-          <Text fontSize='sm' color='#310E3A' marginBottom='100px'>Buen dia!</Text>
+    <ChakraProvider theme={theme}>
+      <>
+        <Flex
+          height="100vh"
+          overflow="hidden"
+          width="100vw"
+        >
+          <Flex
+            flex="0 0 auto"
+            width="180px"
+            flexDirection='column'
+            justifyContent='flex-start'
+            alignItems='center'
+            backgroundImage="../src/assets/img/nav_background.png"
+            backgroundSize='65%'
+            backgroundPosition='left bottom'
+            backgroundRepeat='no-repeat'
+            paddingTop="0"
+            paddingBottom="0"
+            margin="0"
+          >
+            <Box
+              display='flex'
+              flexDirection='column'
+              alignItems='center'
+              justifyContent='center'
+              width='100%'
+              marginTop='0' 
+              marginBottom='0'
+              paddingBottom="30px"
+              paddingTop="15px"
+            >
+              <Image src='/src/assets/img/dif_logo.png' h='40px' alt='DIF Xalapa' marginBottom='16px' />
+              <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov' size='xl' marginBottom='8px' />
+              <Text fontSize='md' color='#310E3A'>Nombre Usuario</Text>
+              <Text fontSize='xs' color='#310E3A'>Buen dia!</Text>
+            </Box>
 
-          <Box alignItems='left'>
-            <Text marginY='10px'>Dashboard</Text>
-            <Text marginY='10px'>Solicitudes</Text>
-            <Text marginY='10px'>Inventario</Text>
-            <Text marginY='10px'>Reportes</Text>
-            <Text marginY='10px'>Configuraciones</Text>
-            <Text marginY='10px'>Cerrar sesion</Text>
+
+            <Flex flexDirection='column' alignItems='center' width='100%'>
+              <Button onClick={changeCategory("dashboard")} leftIcon={<FaHome />} width='100%' sx={buttonStyle}>Dashboard</Button>
+              <Button onClick={changeCategory("enProceso")} leftIcon={<FaBox />} width='100%' sx={buttonStyle}>Solicitudes</Button>
+              <Button onClick={changeCategory("enProceso")} leftIcon={<FaFileAlt />} width='100%' sx={buttonStyle}>Inventario</Button>
+              <Button onClick={changeCategory("enProceso")} leftIcon={<FaChartBar />} width='100%' sx={buttonStyle}>Reportes</Button>
+              <Button onClick={changeCategory("enProceso")} leftIcon={<FaTools />} width='100%' sx={buttonStyle}>Configuraciones</Button>
+              <Button onClick={changeCategory("enProceso")} leftIcon={<FaPowerOff />} width='100%' sx={buttonStyle}>Cerrar sesión</Button>
+            </Flex>
+
+          </Flex>
+
+          <Box flex="1">
+          {renderContent()}
           </Box>
-        </Box>
-
-        <Box flex="1">
-          Segunda columna
-        </Box>
-      </Flex>
-    </>
+        </Flex>
+      </>
+    </ChakraProvider>
   )
 }
 
